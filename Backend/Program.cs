fb79fc84cec829635849d76afa78dbae92499c8c
Nabhan <nabedi2006@gmail.com>
2025-12-13 23:14:10 -0500

    added docker-compose to containerize backend service and postgresql db

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -73,6 +73,13 @@ builder.Services.AddScoped<IRequestsService, RequestsService>();
 
 var app = builder.Build();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LibraryAppContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
